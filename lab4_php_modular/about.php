2160a6822831ead3277cc96630fcad8c9c75be36
REZZA <rezzaf205@gmail.com>
2023-04-03 18:19:20 +0700

    File ditambahkan

---
+++ lab4_php_modular/about.php
@@ -0,0 +1,8 @@
+<?php require('header.php'); ?>
+<div class="container">
+    <h2>About</h2>
+    <div class="box">
+        <p>Saya Rezza Fauzi mengucapkan terimakasih.</p>
+    </div>
+</div>
+<?php require('footer.php'); ?>
---
+++ lab4_php_modular/kontak.php
@@ -0,0 +1,13 @@
+<?php require('header.php'); ?>
+<div class="container">
+    <h2>Kontak</h2>
+    <div class="box">
+        <p>Nama : Rezza Fauzi</p>
+        <p>Nim : 312110133</p>
+        <p>Kelas : TI.21.A.1</p>
+        <p>Matkul : Pemograman Web 2</p>
+        <p>Email : rezzaf205@gmail.com</p>
+        <p>No. Telp : 085884621455</p>
+    </div>
+</div>
+<?php require('footer.php'); ?>
